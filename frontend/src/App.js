652c50517aa6c91784c99e17c2c77bc09d8be668
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-03-10 20:06:33 +0530

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+
 const App = () => {
-  return <>hi</>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
